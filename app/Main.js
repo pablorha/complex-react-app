146b2223f68c4898a545d42036a3b727a8b4dc9d
Pablo Sazo <pablo@ripehouseadvisory.com.au>
2022-05-24 16:59:26 +1000

    Stuck

--- app/Main.js
+++ app/Main.js
@@ -1,24 +1,32 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import { createRoot } from 'react-dom/client'
 const container = document.getElementById('app')
 const root = createRoot(container)
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Axios from 'axios'
+Axios.defaults.baseURL = 'http://localhost:8080'
 
 // My Components
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Home from './components/Home'
 import HomeGuest from './components/HomeGuest'
 import About from './components/About'
 import Terms from './components/Terms'
+import CreatePost from './components/CreatePost'
+import ViewSinglePost from './components/ViewSinglePost'
 
 function Main() {
+	const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('complexappToken')))
 	return (
 		<Router>
-			<Header />
+			<Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
 			<Routes>
-				<Route path='/' element={<HomeGuest />} />
+				<Route path='/' element={loggedIn ? <Home /> : <HomeGuest />} />
 				<Route path='/about-us' element={<About />} />
 				<Route path='/terms' element={<Terms />} />
+				<Route path='/create-post' element={<CreatePost />} />
+				<Route path='/post/:id' element={<ViewSinglePost />} />
 			</Routes>
 			<Footer />
 		</Router>
