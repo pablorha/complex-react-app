97cadda6a056e0153bb44af3a5b91f9b53e69b9d
Pablo Sazo <pablo@ripehouseadvisory.com.au>
2022-05-26 09:29:59 +1000

    immer

--- app/Main.js
+++ app/Main.js
@@ -1,7 +1,6 @@
 import React, { useState, useReducer } from 'react'
 import { createRoot } from 'react-dom/client'
-const container = document.getElementById('app')
-const root = createRoot(container)
+import { useImmerReducer } from 'use-immer'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Axios from 'axios'
 Axios.defaults.baseURL = 'http://localhost:8080'
@@ -20,23 +19,29 @@ import CreatePost from './components/CreatePost'
 import ViewSinglePost from './components/ViewSinglePost'
 import FlashMessages from './components/FlashMessages'
 
+const container = document.getElementById('app')
+const root = createRoot(container)
+
 function Main() {
 	const initialState = {
 		loggedIn: Boolean(localStorage.getItem('complexappToken')),
 		flashMessages: [],
 	}
 
-	function ourReducer(state, action) {
+	function ourReducer(draft, action) {
 		switch (action.type) {
 			case 'login':
-				return { loggedIn: true, flashMessages: state.flashMessages }
+				draft.loggedIn = true
+				return
 			case 'logout':
-				return { loggedIn: false, flashMessages: state.flashMessages }
+				draft.loggedIn = false
+				return
 			case 'flashMessage':
-				return { loggedIn: state.loggedIn, flashMessages: state.flashMessages.concat(action.value) }
+				draft.flashMessages.push(action.value)
+				return
 		}
 	}
-	const [state, dispatch] = useReducer(ourReducer, initialState)
+	const [state, dispatch] = useImmerReducer(ourReducer, initialState)
 
 	return (
 		<StateContext.Provider value={state}>
