ac2f5344c124bd3f4c31a975603bc7971f97b2d4
Pablo Sazo <pablo@ripehouseadvisory.com.au>
2022-05-27 13:56:27 +1000

    Delete posts

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import Page from './Page'
 import { useParams, Link, useNavigate } from 'react-router-dom'
 import Axios from 'axios'
@@ -6,8 +6,12 @@ import LoadinDotsIcon from './LoadinDotsIcon'
 import ReactMarkdown from 'react-markdown'
 import ReactTooltip from 'react-tooltip'
 import NotFound from './NotFound'
+import StateContext from '../StateContext'
+import DispatchContext from '../DispatchContext'
 
-function ViewSinglePost() {
+function ViewSinglePost(props) {
+	const appState = useContext(StateContext)
+	const appDispatch = useContext(DispatchContext)
 	const { id } = useParams()
 	const [isLoading, setIsLoading] = useState(true)
 	const [post, setPost] = useState()
@@ -46,20 +50,46 @@ function ViewSinglePost() {
 	const date = new Date(post.createdDate)
 	const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
 
+	function isOwner() {
+		if (appState.loggedIn) {
+			return appState.user.username == post.author.username
+		}
+		return false
+	}
+
+	async function deleteHandler() {
+		const areYouSure = window.confirm('Do you really want to delete this post?')
+		if (areYouSure) {
+			try {
+				const response = await Axios.delete(`/post/${id}`, { data: { token: appState.user.token } })
+				if (response.data == 'Success') {
+					// 1. Display a flash Message
+					appDispatch({ type: 'flashMessage', value: 'Post was successfully deleted.' })
+					// 2. redirect back to the user's profile.
+					navigate(`/profile/${appState.user.username}`)
+				}
+			} catch (e) {
+				console.log('The was a porblem')
+			}
+		}
+	}
+
 	return (
 		<Page title={post.title}>
 			<div className='d-flex justify-content-between'>
 				<h2>{post.title}</h2>
-				<span className='pt-2'>
-					<Link to={`/post/${post._id}/edit`} data-tip='Edit' data-for='edit' className='text-primary mr-2'>
-						<i className='fas fa-edit'></i>
-					</Link>
-					<ReactTooltip id='edit' className='custom-tooltip' />
-					<a data-tip='Delete' data-for='delete' className='delete-post-button text-danger'>
-						<i className='fas fa-trash'></i>
-					</a>
-					<ReactTooltip id='delete' className='custom-tooltip' />
-				</span>
+				{isOwner() && (
+					<span className='pt-2'>
+						<Link to={`/post/${post._id}/edit`} data-tip='Edit' data-for='edit' className='text-primary mr-2'>
+							<i className='fas fa-edit'></i>
+						</Link>
+						<ReactTooltip id='edit' className='custom-tooltip' />
+						<a onClick={deleteHandler} data-tip='Delete' data-for='delete' className='delete-post-button text-danger'>
+							<i className='fas fa-trash'></i>
+						</a>
+						<ReactTooltip id='delete' className='custom-tooltip' />
+					</span>
+				)}
 			</div>
 
 			<p className='text-muted small mb-4'>
